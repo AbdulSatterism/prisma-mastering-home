dcd939712a04acf84f4fd06d662755c8710ae022
AbdulSatterism <abdulsatter.ism@gmail.com>
2025-02-01 23:39:28 +0600

    first commit

---
+++ src/index.ts
@@ -0,0 +1,31 @@
+import { PrismaClient } from "@prisma/client";
+
+
+const prisma = new PrismaClient()
+
+const main = async ()=>{
+    //* create data in the table 
+    // const result = await prisma.post.create({
+    //     data:{
+    //         title:"This is an title 2",
+    //         content:"Second content here",
+    //         authorName:"Md. Abdul Satter"
+    //     }
+    // })
+
+
+    //TODO: get all data 
+
+    //!error 
+     
+    //* get all data 
+    // const result = await prisma.post.findMany()
+
+    //* console.log(result)
+
+
+
+}
+
+
+main()
