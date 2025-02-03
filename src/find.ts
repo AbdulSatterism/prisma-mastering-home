9775e06429a0481b9c61a0cb2c86e55af6ea58e2
AbdulSatterism <abdulsatter.ism@gmail.com>
2025-02-03 20:27:28 +0600

    implement find query in find.ts file

---
+++ src/find.ts
@@ -0,0 +1,37 @@
+import { PrismaClient } from "@prisma/client";
+
+
+const prisma = new PrismaClient()
+
+const main = async ()=>{
+
+
+    //TODO: get all data 
+     
+    //* find all data  
+    const result = await prisma.post.findMany()
+
+    //! find first method throw error if not available
+    //* findFirst user for get match first element
+    const findFirst = await prisma.post.findFirstOrThrow({
+        where:{
+            id:2
+        }
+    })
+
+//* findUnique use for only find by unique field otherwise throw error
+
+const findUnique= await prisma.post.findUniqueOrThrow({
+    where:{
+        id:2
+    }
+})
+
+console.log(findUnique)
+
+
+
+}
+
+
+main()
