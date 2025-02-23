d767b4573479217d98cc95746c0f54aebc10211e
AbdulAbdulSatterism <abdulsatter.ism@gmail.com>
2025-02-23 20:05:05 +0600

    updated

---
+++ src/lab.ts
@@ -0,0 +1,13 @@
+//  CREATE TABLE employe (
+//      INT,
+//     Name VARCHAR(50),
+//     Sal DECIMAL(10, 2),
+//     Cit VARCHAR(50)
+// );
+
+// INSERT INTO employe (Em_No, Name, Sal, Cit) VALUES
+// (1, 'Hasan', 30000, 'Jamal ur'),
+// (2, 'Habib', 7000, 'New York'),
+// (3, 'Saiful', 14000, 'Gazi ur'),
+// (4, 'Amir', 17000, 'Khulna'),
+// (5, 'Mahmud', 15000, 'New York');
